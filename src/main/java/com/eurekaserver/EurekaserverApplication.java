891430571bd759cff615e69977ee077e64138550
Yogesh Choudhary <yogesh.choudhary@neosoftmail.com>
2023-09-06 22:47:30 +0530

    add eureka server

---
+++ src/main/java/com/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,18 @@
+package com.eurekaserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@Slf4j
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+		log.info("Eurekaserver Application Running on http://localhost:8761");
+	}
+
+}
